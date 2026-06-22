f3aaa847a39d1b1b80c6aad30c95a2221b8b553a
bm295 <baominh.nguyen.295@gmail.com>
2026-06-22 19:56:26 +0700

    Add API-compatible enum names

--- src/main/java/com/managementplatform/domain/enums/PaymentStatus.java
+++ src/main/java/com/managementplatform/domain/enums/PaymentStatus.java
@@ -1,6 +1,16 @@
 package com.managementplatform.domain.enums;
 
 public enum PaymentStatus {
-    FAILED,
-    SUCCEEDED
+    FAILED("Failed"),
+    SUCCEEDED("Succeeded");
+
+    private final String apiName;
+
+    PaymentStatus(String apiName) {
+        this.apiName = apiName;
+    }
+
+    public String apiName() {
+        return apiName;
+    }
 }
